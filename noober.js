b2021e192ca581eba161f997e967e725dade5c68
John Corbett <john.corbett@kellogg.northwestern.edu>
2021-02-01 22:25:13 -0600

    Got it working

--- noober.js
+++ noober.js
@@ -1,8 +1,3 @@
-// function appendQuote(rides) {
-
-// }
-
-
 async function pageLoaded() {
   let response = await fetch('https://kiei451.com/api/rides.json')
   let json = await response.json()
@@ -10,33 +5,189 @@ async function pageLoaded() {
   // writes the returned JSON to the console
   console.dir(json)
 
-  // for (let i = 0; i<json.length; i++) {
-  //   let text = ride[i]
-  //   document.write(`
-  //   <div class="text-center">
-  //     ${text}
-  //     </div>
-  //     `)
-  //   }
-  
   // 🔥 start here: write code to loop through the rides
   for (let i = 0; i < json.length; i++) {
-    let rides = json[i]  
+    let ride = json[i]  
 
-    for (let i = 0; i < rides.length; i++) {   
+      if (ride.length > 1) {
+        levelofService = "Noober Pool"
 
-      let passengerName = `${rides[i].passengerDetails.first} ${rides[i].passengerDetails.last}`
-      let passengerPhone = rides[i].passengerDetails.phoneNumber
-      let passengerPickup1 = rides[i].pickupLocation.address
-      let passengerPickup2 = `${rides[i].pickupLocation.city}, ${rides[i].pickupLocation.state} ${rides[i].pickupLocation.zip}`
-      let passengerDropoff1 = rides[i].dropoffLocation.address
-      let passengerDropoff2 = `${rides[i].dropoffLocation.city}, ${rides[i].dropoffLocation.state} ${rides[i].dropoffLocation.zip}`
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+              <i class="fas fa-car-side"></i>
+              <span>${levelofService}</span>
+            </h1>
+            `)
+
+        for (let i = 0; i < ride.length; i++) {
+
+          let passengerName = `${ride[i].passengerDetails.first} ${ride[i].passengerDetails.last}`
+          let passengerPhone = ride[i].passengerDetails.phoneNumber
+          let passengerNumberOfPassengers = ride[i].numberOfPassengers
+          let passengerPickup1 = ride[i].pickupLocation.address
+          let passengerPickup2 = `${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}`
+          let passengerDropoff1 = ride[i].dropoffLocation.address
+          let passengerDropoff2 = `${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}`
+
+          document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+        
+            <div class="border-4 border-gray-900 p-4 my-4 text-left">
+              <div class="flex">
+                <div class="w-1/2">
+                  <h2 class="text-2xl py-1">${passengerName}</h2>
+                  <p class="font-bold text-gray-600">${passengerPhone}</p>
+                </div>
+                <div class="w-1/2 text-right">
+                  <span class="rounded-xl bg-gray-600 text-white p-2">
+                  ${passengerNumberOfPassengers} passengers
+                  </span>
+                </div>
+              </div>
+              <div class="mt-4 flex">
+                <div class="w-1/2">
+                  <div class="text-sm font-bold text-gray-600">PICKUP</div>
+                  <p>${passengerPickup1}</p>
+                  <p>${passengerPickup2}</p>
+                </div>
+                <div class="w-1/2">
+                  <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+                  <p>${passengerDropoff1}</p>
+                  <p>${passengerDropoff2}</p>
+                </div>
+              </div>
+            </div>
+
+            `)
+        }
+
+      } else if (ride.purpleRequested) {
+        levelofService = "Noober Purple"
+
+
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelofService}</span>
+        </h1>
+        `)
+
+        for (let i = 0; i < ride.length; i++) {
+
+          let passengerName = `${ride[i].passengerDetails.first} ${ride[i].passengerDetails.last}`
+          let passengerPhone = ride[i].passengerDetails.phoneNumber
+          let passengerNumberOfPassengers = ride[i].numberOfPassengers
+          let passengerPickup1 = ride[i].pickupLocation.address
+          let passengerPickup2 = `${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}`
+          let passengerDropoff1 = ride[i].dropoffLocation.address
+          let passengerDropoff2 = `${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}`
+    
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+
+        <div class="border-4 border-purple-500 p-4 my-4 text-left">
+          <div class="flex">
+            <div class="w-1/2">
+              <h2 class="text-2xl py-1">${passengerName}</h2>
+              <p class="font-bold text-gray-600">${passengerPhone}</p>
+            </div>
+            <div class="w-1/2 text-right">
+              <span class="rounded-xl bg-purple-600 text-white p-2">
+              ${passengerNumberOfPassengers} passengers
+              </span>
+            </div>
+          </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${passengerPickup1}</p>
+              <p>${passengerPickup2}</p>
+            </div>
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+              <p>${passengerDropoff1}</p>
+              <p>${passengerDropoff2}</p>
+            </div>
+          </div>
+        </div>
+    
+      `)
+        }
+
+      } else if (ride.numberOfPassengers > 3) {
+        levelofService = "Noober XL"
+
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelofService}</span>
+        </h1>
+        `)
+
+        for (let i = 0; i < ride.length; i++) {
+       
+          let passengerName = `${ride[i].passengerDetails.first} ${ride[i].passengerDetails.last}`
+          let passengerPhone = ride[i].passengerDetails.phoneNumber
+          let passengerNumberOfPassengers = ride[i].numberOfPassengers
+          let passengerPickup1 = ride[i].pickupLocation.address
+          let passengerPickup2 = `${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}`
+          let passengerDropoff1 = ride[i].dropoffLocation.address
+          let passengerDropoff2 = `${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}`
+
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+    
+        <div class="border-4 border-gray-900 p-4 my-4 text-left">
+          <div class="flex">
+            <div class="w-1/2">
+              <h2 class="text-2xl py-1">${passengerName}</h2>
+              <p class="font-bold text-gray-600">${passengerPhone}</p>
+            </div>
+            <div class="w-1/2 text-right">
+              <span class="rounded-xl bg-gray-600 text-white p-2">
+              ${passengerNumberOfPassengers} passengers
+              </span>
+            </div>
+          </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${passengerPickup1}</p>
+              <p>${passengerPickup2}</p>
+            </div>
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+              <p>${passengerDropoff1}</p>
+              <p>${passengerDropoff2}</p>
+            </div>
+          </div>
+        </div>
+
+        `) 
+        }
+
+      } else {
+        levelofService = "Noober X" 
 
       document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+
       <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
       <i class="fas fa-car-side"></i>
-      <span>Noober X</span>
+      <span>${levelofService}</span>
     </h1>
+    `)
+
+    for (let i = 0; i < ride.length; i++) {
+
+      let passengerName = `${ride[i].passengerDetails.first} ${ride[i].passengerDetails.last}`
+      let passengerPhone = ride[i].passengerDetails.phoneNumber
+      let passengerNumberOfPassengers = ride[i].numberOfPassengers
+      let passengerPickup1 = ride[i].pickupLocation.address
+      let passengerPickup2 = `${ride[i].pickupLocation.city}, ${ride[i].pickupLocation.state} ${ride[i].pickupLocation.zip}`
+      let passengerDropoff1 = ride[i].dropoffLocation.address
+      let passengerDropoff2 = `${ride[i].dropoffLocation.city}, ${ride[i].dropoffLocation.state} ${ride[i].dropoffLocation.zip}`
+
+    document.querySelector('.rides').insertAdjacentHTML('beforeend', `
     
     <div class="border-4 border-gray-900 p-4 my-4 text-left">
       <div class="flex">
@@ -46,7 +197,7 @@ async function pageLoaded() {
         </div>
         <div class="w-1/2 text-right">
           <span class="rounded-xl bg-gray-600 text-white p-2">
-            2 passengers
+            ${passengerNumberOfPassengers} passengers
           </span>
         </div>
       </div>
@@ -63,141 +214,20 @@ async function pageLoaded() {
         </div>
       </div>
     </div>
-    
-    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          <i class="fas fa-car-side"></i>
-          <span>Noober Pool</span>
-        </h1>
-    
-        <div class="border-4 border-gray-900 p-4 my-4 text-left">
-          <div class="flex">
-            <div class="w-1/2">
-              <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
-              <p class="font-bold text-gray-600">(312) 555-1212</p>
-            </div>
-            <div class="w-1/2 text-right">
-              <span class="rounded-xl bg-gray-600 text-white p-2">
-                1 passengers
-              </span>
-            </div>
-          </div>
-          <div class="mt-4 flex">
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">PICKUP</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-          </div>
-        </div>
-    
-        <div class="border-4 border-gray-900 p-4 my-4 text-left">
-          <div class="flex">
-            <div class="w-1/2">
-              <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
-              <p class="font-bold text-gray-600">(312) 555-1212</p>
-            </div>
-            <div class="w-1/2 text-right">
-              <span class="rounded-xl bg-gray-600 text-white p-2">
-                1 passengers
-              </span>
-            </div>
-          </div>
-          <div class="mt-4 flex">
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">PICKUP</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-          </div>
-        </div>
-    
-        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          <i class="fas fa-car-side"></i>
-          <span>Noober XL</span>
-        </h1>
-    
-        <div class="border-4 border-gray-900 p-4 my-4 text-left">
-          <div class="flex">
-            <div class="w-1/2">
-              <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
-              <p class="font-bold text-gray-600">(312) 555-1212</p>
-            </div>
-            <div class="w-1/2 text-right">
-              <span class="rounded-xl bg-gray-600 text-white p-2">
-                5 passengers
-              </span>
-            </div>
-          </div>
-          <div class="mt-4 flex">
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">PICKUP</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-          </div>
-        </div>
-    
-        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          <i class="fas fa-car-side"></i>
-          <span>Noober Purple</span>
-        </h1>
-    
-        <div class="border-4 border-purple-500 p-4 my-4 text-left">
-          <div class="flex">
-            <div class="w-1/2">
-              <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
-              <p class="font-bold text-gray-600">(312) 555-1212</p>
-            </div>
-            <div class="w-1/2 text-right">
-              <span class="rounded-xl bg-purple-600 text-white p-2">
-                1 passengers
-              </span>
-            </div>
-          </div>
-          <div class="mt-4 flex">
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">PICKUP</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-            <div class="w-1/2">
-              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
-              <p>123 Main St</p>
-              <p>Chicago, IL 60603</p>
-            </div>
-          </div>
-        </div>
-    
-      `)
 
+    `)
+    }
 
 
       // appendQuote(rides)
 
       
-
-
-
         // put in the function(ride) to add it to the HTML output
-      } 
+      // } 
     }
 
 
+}
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
